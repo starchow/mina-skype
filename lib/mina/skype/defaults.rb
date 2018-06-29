adba135af9c8c1153bf9d4dfb1372cc5367de443
Nam Pham <nam.pham@inspireventures.com>
2018-06-29 14:33:46 +0700

    Remove set_default, mina removed it

--- lib/mina/skype/defaults.rb
+++ lib/mina/skype/defaults.rb
@@ -1,11 +1,11 @@
 # Required
-set_default :skype_app_id,          -> { ENV['SKYPE_APP_ID'] }
-set_default :skype_app_secret,      -> { ENV['SKYPE_APP_SECRET'] }
-set_default :skype_conversation_id, -> { ENV['SKYPE_CONVERSATION_ID'] }
+set :skype_app_id,          -> { ENV['SKYPE_APP_ID'] } unless set?(:skype_app_id)
+set :skype_app_secret,      -> { ENV['SKYPE_APP_SECRET'] } unless set?(:skype_app_secret)
+set :skype_conversation_id, -> { ENV['SKYPE_CONVERSATION_ID'] } unless set?(:skype_conversation_id)
 
 # Optional
-set_default :skype_application,     -> { ENV['SKYPE_APPLICATION'] || application }
+set :skype_application,     -> { ENV['SKYPE_APPLICATION'] || application } unless set?(:skype_application)
 
 # Git
-set_default :deployer,              -> { ENV['GIT_AUTHOR_NAME'] || %x[git config user.name].chomp }
-set_default :deployed_revision,     -> { ENV['GIT_COMMIT'] || %x[git rev-parse #{branch}].strip }
+set :deployer,              -> { ENV['GIT_AUTHOR_NAME'] || %x[git config user.name].chomp } unless set?(:deployer)
+set :deployed_revision,     -> { ENV['GIT_COMMIT'] || %x[git rev-parse #{branch} unless set?()].strip } unless set?(:deployed_revision)
