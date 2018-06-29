11fb47f1b45704fdbe7ec55a338545dfcba74775
Nam Pham <nam.pham@inspireventures.com>
2018-06-29 14:46:47 +0700

    Fix setting issue

--- lib/mina/skype/defaults.rb
+++ lib/mina/skype/defaults.rb
@@ -4,8 +4,8 @@ set :skype_app_secret,      -> { ENV['SKYPE_APP_SECRET'] } unless set?(:skype_ap
 set :skype_conversation_id, -> { ENV['SKYPE_CONVERSATION_ID'] } unless set?(:skype_conversation_id)
 
 # Optional
-set :skype_application,     -> { ENV['SKYPE_APPLICATION'] || application } unless set?(:skype_application)
+set :skype_application,     -> { ENV['SKYPE_APPLICATION'] || fetch(:application) } unless set?(:skype_application)
 
 # Git
 set :deployer,              -> { ENV['GIT_AUTHOR_NAME'] || %x[git config user.name].chomp } unless set?(:deployer)
-set :deployed_revision,     -> { ENV['GIT_COMMIT'] || %x[git rev-parse #{branch} unless set?()].strip } unless set?(:deployed_revision)
+set :deployed_revision,     -> { ENV['GIT_COMMIT'] || %x[git rev-parse #{fetch(:branch)} unless set?()].strip } unless set?(:deployed_revision)
--- lib/mina/skype/version.rb
+++ lib/mina/skype/version.rb
@@ -1,5 +1,5 @@
 module Mina
   module Skype
-    VERSION = '0.1.3'
+    VERSION = '0.1.4'
   end
 end
